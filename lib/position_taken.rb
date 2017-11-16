132b7b9b99085e33da916ebbd975cb7543d42f06
Adil Abed <dr.sivana@protonmail.com>
2017-11-16 15:22:03 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken(board , index)
+  if board[index - 1] == "" && board[index - 1] == " " && board[index - 1] == nil
+    "false"
+  elsif board[index - 1] == "X" && board[index - 1] == "O"
+    "true"
+    
+    
+  end
+  
+end
